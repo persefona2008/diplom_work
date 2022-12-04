976a98948cbef97e91af0d015d99cae223c4746b
persefona2008 <115982953+persefona2008@users.noreply.github.com>
2022-12-05 03:29:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,112 +1,87 @@
 import requests
-from pprint import pprint
+import os
 import json
-import datetime
+import configparser
+import time
+from tqdm import tqdm
 
-token = "TOKEN "
-URL = 'https://api.vk.com/method/account.getProfileInfo'
-params = {
-    'access_token': token,
-    'v': '5.131'
-}
-res_1 = requests.get(URL, params=params)
-pprint(res_1.json())
-res_2 = res_1.json()
-personal_id = str(res_2['response']['id'])
-# print(res_2)
+config = configparser.ConfigParser()
+config.read("1.ini")
 
-URL = 'https://api.vk.com/method/photos.get'
-params = {
-    'owner_id': f'{personal_id}',
-    'album_id': 'wall',
-    'extended': 1,
-    'photo_sizes': 1,
-    'access_token': token,
-    'v': '5.131'
-}
-res = requests.get(URL, params=params)
-result = res.json()
-foto_list = result['response']['items']
-# print(result)
-
-def time_convert(time_unix):
-    time_bc = datetime.datetime.fromtimestamp(time_unix)
-    str_time = time_bc.strftime('%Y-%m-%d time %H-%M-%S')
-    return str_time
+TOKEN_VK = config['vk_api']['access_token']
+TOKEN_YADISK = config['yadisk_api']['api_token']
 
 
-# ссылка на максимальный файл.
-def geting_fotos_links(list_of_size):
-    links_of_foto = ''
-    max_size_foto = 0
-    for any_foto in list_of_size:
-        if any_foto['height'] > max_size_foto:
-            max_size_foto = any_foto['height']
-            links_of_foto = any_foto['url']
-    return links_of_foto
+class UserService:
+
+    def __init__(self, user_id, token: str):
+        self.user_id = user_id
+        self.token = token
+        self.api_version = config['vk_api']['api_version']
+        self.get_photos_method_url = config['vk_api']['get_photos_method_url']
+        self.download_file_path = config['files_path']['download_file_path']
+        self.get_upload_url_api = config['yadisk_api']['get_upload_url_api']
+        self.file_path = config['files_path']['download_file_path']
+        self.mkdir_url = config['yadisk_api']['mkdir_url']
+
+    def _get_photos_from_folder(self) -> list:
+        file_list = os.listdir(self.file_path)
+        return file_list
+
+    def get_photos_method(self, user_id):
+        params = {'access_token': self.token,
+                  'v': self.api_version,
+                  'album_id': 'profile',
+                  'owner_id': user_id,
+                  'extended': True,
+                  'photo_sizes': True
+                  }
+        response = requests.get(self.get_photos_method_url, params=params)
+        profile_list = response.json()
+
+        for file in tqdm(profile_list['response']['items']):
+            time.sleep(3)
+            self.size = file['sizes'][-1]['type']
+            photo_url = file['sizes'][-1]['src']
+            file_name = file['likes']['count']
+            download_photo = requests.get(photo_url)
+            with open(f'{self.download_file_path}/{file_name}.jpg', 'wb') as f:
+                f.write(download_photo.content)
+
+    def create_folder(self):
+        params = {'path': self.file_path}
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': TOKEN_YADISK}
+        create_dir = requests.api.put(self.mkdir_url, headers=headers, params=params)
+
+    def upload_photo(self):
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': TOKEN_YADISK}
+        logs_list = []
+
+        for photo in tqdm(self._get_photos_from_folder()):
+            time.sleep(3)
+            params = {'path': f'{self.file_path}/{photo}'}
+            get_upload_url = requests.get(self.get_upload_url_api, headers=headers, params=params)
+            get_url = get_upload_url.json()
+            upload_url = get_url['href']
+            file_upload = requests.api.put(upload_url, data=open(f'{self.file_path}/{photo}', 'rb'), headers=headers)
+            status = file_upload.status_code
+
+            download_log = {'file_name': photo, 'size': self.size}
+            logs_list.append(download_log)
+
+        with open('files/log.json', 'a') as file:
+            json.dump(logs_list, file, indent=2)
+
+        if 500 > status != 400:
+            print('Фотографии успешно загружены!')
+        else:
+            print('Ошибка при загрузке фотографий')
 
 
-# Получаем размеры файла
-def geting_max_size_foto(list_of_size):
-    size_of_foto = ''
-    max_size_foto = 0
-    for any_foto in list_of_size:
-        if any_foto['height'] > max_size_foto:
-            max_size_foto = any_foto['height']
-            max_size_foto_2 = any_foto['width']
-            size_of_foto = f'h={max_size_foto} X  w={max_size_foto_2}'
-    return size_of_foto
-
-
-# Получаем название файла.
-def making_info_for_foto(any_foto):
-    count_likes = any_foto['likes']['count']
-    date_foto = any_foto['date']
-    name_of_file = f'{count_likes} {date_foto}'
-    size_of_file = geting_max_size_foto(any_foto['sizes'])
-    any_foto_info = {"file_name": name_of_file, "size": size_of_file}
-    return any_foto_info
-
-
-result_for_every_foto = []
-some_fotos_links = []
-for foto_in_list in foto_list:
-    result_for_every_foto.append(making_info_for_foto(foto_in_list))
-    some_fotos_links.append(geting_fotos_links(foto_in_list['sizes']))
-
-pprint(result_for_every_foto)  # печать файла с результатами
-pprint(some_fotos_links)  # печать ссылок фото
-
-
-class YaUploader:  # Класс записи объекта на диск яндекса
-    def __init__(self, token_ya: str):
-        self.token_ya = token_yandex
-
-    def get_headers(self):
-        return {
-            'Content-Type': 'application/json',
-            'Authorization': 'OAuth {}'.format(self.token_ya)
-        }
-
-    def get_upload_file(self, file_link, disk_file_path):
-        headers = self.get_headers()
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        params = {"url": file_link, "path": disk_file_path, "overwrite": False}
-        r = requests.post(url=upload_url, params=params, headers=headers)
-        res = r.json()
-        pprint(res)
-
-
-token_ya = 'token_yandex'
-putloader = YaUploader(token_ya)
-index = 0
-name_foto = ''
-for one_foto in some_fotos_links:  # запись фото на яндекс диск
-    n_foto = one_foto
-    name_foto = result_for_every_foto[index]['file_name']
-    path_yandex = f'/photo/{name_foto}'
-    putloader.get_upload_file(n_foto, path_yandex)
-    index += 1
-
-with open('result_file.json', 'w') as f:  # Запись результа в корневой каталог файла .
-    json.dump(result_for_every_foto, f, indent=2)
+if __name__ == '__main__':
+    user1 = UserService(1742449, TOKEN_VK)
+    save_photos = user1.get_photos_method(user1.user_id)
+    user1.create_folder()
+    back_up = user1.upload_photo()
